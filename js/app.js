8c62355f2d4f6bcaf518d7d101b15fd1958b31c4
Franziskus Nakajima <franziskus.nakajima@gmail.com>
2017-01-04 10:42:40 +0100

    feat: Adds list of cats to click through.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-var Cat = function(){
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable("Kitty");
-  this.nicknames = ko.observableArray(["Sweetie", "Kitty", "Bay-B", "Furball", "Scratchie"]);
-  this.imgSrc = ko.observable("img/22252709_010df3379e_z.jpg");
-  this.imageAttribution = ko.observable("photourl");
+var Cat = function(data){
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.nicknames = ko.observableArray(data.nicknames);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.imageAttribution = ko.observable(data.imageAttribution);
 
   this.levelNo = ko.computed(function(){
     return Math.floor(this.clickCount()/10)
@@ -14,18 +14,70 @@ var Cat = function(){
     "Kitten",
     "Cat-Teen",
     "Cat on the prowl",
-    "Big Kitty"
+    "Big Kitty",
+    "Cat Royalty"
   ]);
 
   this.level  = ko.computed(function(){
-    return this.levelNo() >= 5? "Cat Royalty": this.levels()[this.levelNo()];
+    return this.levelNo() >= 5? this.levels()[5] : this.levels()[this.levelNo()];
   }, this);
 }
 
+initialCats = [
+  {
+    clickCount: 0,
+    name: "Kitty",
+    nicknames: ["Sweetie", "Kitty", "Bay-B", "Furball", "Scratchie"],
+    imgSrc: "img/22252709_010df3379e_z.jpg",
+    imageAttribution: "photourl"
+  },
+  {
+    clickCount: 0,
+    name: "Gingy",
+    nicknames: ["Barbarossa", "Red Baron"],
+    imgSrc: "img/434164568_fea0ad4013_z.jpg",
+    imageAttribution: "photourl"
+  },
+  {
+    clickCount: 0,
+    name: "Boss",
+    nicknames: ["Yakuza", "Al Capone", "Clarence"],
+    imgSrc: "img/1413379559_412a540d29_z.jpg",
+    imageAttribution: "photourl"
+  },
+  {
+    clickCount: 0,
+    name: "Simba",
+    nicknames: ["King", "Puma"],
+    imgSrc: "img/4154543904_6e2428c421_z.jpg",
+    imageAttribution: "photourl"
+  },
+  {
+    clickCount: 0,
+    name: "Sleepy",
+    nicknames: ["Yawnz"],
+    imgSrc: "img/9648464288_2516b35537_z.jpg",
+    imageAttribution: "photourl"
+  }
+]
+
 var ViewModel = function() {
-  this.currentCat = ko.observable(new Cat());
+  var self = this;
+
+  this.CatList = ko.observableArray([]);
+
+  initialCats.forEach(function(catItem){
+    self.CatList.push(new Cat(catItem));
+  });
+
+  this.currentCat = ko.observable(self.CatList()[0]);
+
   this.incrementCounter = function() {
-    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    this.clickCount(this.clickCount() + 1);
+  }
+
+  this.setCurrentCat = function() {
+    self.currentCat(this);
   }
 }
 
